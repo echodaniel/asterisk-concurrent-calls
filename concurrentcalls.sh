07ef558f47734907b75b85e3ae7f99a89b4f5966
echodaniel <31143925+echodaniel@users.noreply.github.com>
2018-05-14 15:43:28 -0600

    Changed script description, also changed 'tee -a' to 'tee' to see if that changes the csv output.

--- concurrentcalls.sh
+++ concurrentcalls.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#script to monitor concurrent call channels and output to a CSV file. testing only right now.
+#Script to monitor concurrent call channels and output to a CSV file. In testing right now, though the script below does work.
 watch -t -n 10 "(date '+TIME,%Y-%m-%d,%H:%M:%S' ; ps aux | asterisk -rx 'core show channels' | egrep 'SIP/[[:digit:]]' | wc -l) | tee -a cclist.csv"
