9e166eeb3ae13159346517c19f9b583c0d47fc20
echodaniel <31143925+echodaniel@users.noreply.github.com>
2018-05-15 14:44:02 -0600

    Update and rename concalls.sh to callconcurrency.sh
    Adjusted script to match main branch.

---
+++ callconcurrency.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#script to monitor concurrent SIP call channels and output to a CSV file. testing only right now.
+watch -t -n 10 "(date '+TIME,%Y-%m-%d,%H:%M:%S' ; ps aux | asterisk -rx 'core show channels' | egrep 'SIP/[[:digit:]]' | wc -l) | xargs |sed -e 's/ /\,/g' | tee -a callconcurrency.csv"
