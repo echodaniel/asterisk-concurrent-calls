99cd740a4fd49ea88d3e43f535d6849d261b8181
echodaniel <31143925+echodaniel@users.noreply.github.com>
2018-05-15 15:27:13 -0600

    Update callconcurrency.sh
    Added screen and related arguments to command so it can be run with watch and tested output. Runs like a charm and I can watch with screen -r. Reattaching with screen -R kills it. Ready to merge with main branch.

--- callconcurrency.sh
+++ callconcurrency.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script to monitor concurrent SIP call channels and output to a CSV file. testing only right now.
-mmhistogram -t -n 10 "(date '+TIME,%Y-%m-%d,%H:%M:%S' ; ps aux | asterisk -rx 'core show channels' | egrep 'SIP/[[:digit:]]' | wc -l) | tee -a cclist.csv"
+screen -D -m -L -S CallConcurrency -t CallConcurrency watch -t -n 10 "(date '+TIME,%Y-%m-%d,%H:%M:%S' ; ps aux | asterisk -rx 'core show channels' | egrep 'SIP/[[:digit:]]' | wc -l) | xargs |sed -e 's/ /\,/g' | tee -a callconcurrency.csv"
