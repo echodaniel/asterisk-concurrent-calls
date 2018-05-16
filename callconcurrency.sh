a499d7b8f0f65733ee4c6bb7c6393eb34e9e04e5
echodaniel <31143925+echodaniel@users.noreply.github.com>
2018-05-16 10:09:58 -0600

    Update callconcurrency.sh
    Attempting conversion to xlsx file withing script itself. we'll see if it still runs properly.

--- callconcurrency.sh
+++ callconcurrency.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script to monitor concurrent call channels. Outputs date, time, and number of concurrent calls to a CSV file.
-screen -D -m -L -S CallConcurrency -t CallConcurrency watch -t -n 10 "(date '+TIME,%Y-%m-%d,%H:%M:%S' ; ps aux | asterisk -rx 'core show channels' | egrep 'SIP/[[:digit:]]' | wc -l) | xargs |sed -e 's/ /\,/g' | tee -a callconcurrency.csv"
+screen -D -m -L -S CallConcurrency -t CallConcurrency watch -t -n 10 "(date '+TIME,%Y-%m-%d,%H:%M:%S' ; ps aux | asterisk -rx 'core show channels' | egrep 'SIP/[[:digit:]]' | wc -l) | xargs |sed -e 's/ /\,/g' | tee -a callconcurrency.csv" | ssconvert callconcurrency.csv callconcurrency.xlsx
