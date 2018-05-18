f8fecc74e0db119ca6c8d9357ff960600b964153
echodaniel <31143925+echodaniel@users.noreply.github.com>
2018-05-18 09:38:24 -0600

    Update callconcurrency.sh
    There was a space needed between | and sed

--- callconcurrency.sh
+++ callconcurrency.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Script to monitor concurrent call channels. Outputs date, time, and number of concurrent calls to a CSV file.
-screen -D -m -L -S CallConcurrency -t CallConcurrency watch -t -n 10 "(date '+TIME,%Y-%m-%d,%H:%M:%S' ; ps aux | asterisk -rx 'core show channels' | egrep 'SIP/[[:digit:]]' | wc -l) | xargs |sed -e 's/ /\,/g' | tee -a callconcurrency.csv"
+#Script to monitor concurrent call channels. Outputs date, time, and number of concurrent PJSIP calls per 10 seconds to a CSV file.
+screen -D -m -L -S CallConcurrency -t CallConcurrency watch -t -n 10 "(date '+TIME,%Y-%m-%d,%H:%M:%S' ; ps aux | asterisk -rx 'core show channels' | egrep 'SIP/[[:digit:]]' | wc -l) | xargs | sed -e 's/ /\,/g' | tee -a callconcurrency.csv"
